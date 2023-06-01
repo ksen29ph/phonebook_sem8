25ae1415cfdd0a86ddd0cdf6eb64e1eb6ded4770
«Ksenia <ksen29ph@gmail.com>
2023-06-01 22:27:01 +0300

    view done

--- view.py
+++ view.py
@@ -1,2 +1,33 @@
-def welcome():
-    pass # приветствие
+def greetings():
+    print('Добро пожаловать в телефонную книгу!')
+
+def print_menu():
+    print(
+        '1. Вывод телефонной книжки\n'
+        '2. Добавление абонента в телефонную книгу\n'
+        '3. Поиск абонента в телефонной книге\n'
+        '4. Редактирование данных в телефонной книге\n'
+        '5. Удаление данных абонента из телефонной книги\n'
+        '0. Выход из телефонной книги')    
+    
+def show_data():
+    return None
+
+def add_data(fio, phone_number):
+    return None
+
+def find_data(data):
+    return None
+
+def edit_data(phone_book):
+    return None
+
+def del_data(phone_book):
+    return None
+
+def goodbye():
+    print('До новых встреч!')         
+
+def error():
+    print('Неправильно выбрана команда!')   
+
